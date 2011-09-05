788fe92ee668735f28d0345965ffbf5b11de430b
unknown <Dustin@.(none)>
2011-09-06 02:10:32 +0900

    Just implemented basic state switches for visibility markup

--- src/org/jeml/translator/Translator.java
+++ src/org/jeml/translator/Translator.java
@@ -1,5 +1,77 @@
 package org.jeml.translator;
 
-public class Translator {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class Translator {
+	private enum READER_STATE {
+		Visibility, Declaration, MethodCall, Block
+	};
+	
+	private static READER_STATE rstate_ = READER_STATE.Visibility;
+	
+	public static void translateFile(File input, File output) throws FileNotFoundException {
+		FileReader freader = new FileReader(input);
+		BufferedReader reader = new BufferedReader(freader);
+		
+		String line = null;
+		final StringBuilder outstring = new StringBuilder();
+		try {
+			while((line = reader.readLine()) != null) {
+				translate(line, outstring);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static void translate(final String line, final StringBuilder output) {
+		final int length = line.length();
+		
+		int c = 0;
+		while (c < length) {
+			switch (rstate_) {
+				case Visibility:
+					switch (line.charAt(c)) {
+						/** private */
+						case '-':
+							output.append("private ");
+							break;
+							
+						/** public */
+						case '+':
+							output.append("public ");
+							break;
+							
+						/** protected */
+						case '#':
+							output.append("protected ");
+							break;
+							
+						/** static */
+						case '^':
+							output.append("static ");
+							break;
+							
+						/** final */
+						case '!':
+							output.append("final ");
+							break;
+						
+						/** this isn't a visibility declaration, advance the state */
+						default:
+							output.append("private ");
+							output.append(line.charAt(c));
+							rstate_ = READER_STATE.Declaration;
+					}
+					
+					c++;
+					
+					break;
+			}
+		}
+	}
 }
